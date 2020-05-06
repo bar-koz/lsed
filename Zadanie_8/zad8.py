7c4b812bd8878d9936f04d9fc5f7bc42d2854a45
bar-koz <57669415+bar-koz@users.noreply.github.com>
2020-05-06 19:51:55 +0200

    Update Zad8

--- Zadanie_8/zad8.py
+++ Zadanie_8/zad8.py
@@ -17,17 +17,18 @@ classes_name = ['1','2','3']
 x = range(1, 14)
 y = []
 for i in x:
-	pca = PCA(n_components=i)
-	pca.fit(data, classes)
-	y.append(np.mean(pca.explained_variance_ratio_))
-	print np.mean(pca.explained_variance_ratio_)
+	pca = PCA()
+	pca.fit(data[:,:i])
+	y.append(np.sum(np.sqrt(pca.explained_variance_)))
+	print np.sum(np.sqrt(pca.explained_variance_))
 
-pca = PCA(n_components=2)
-pca.fit(data[:,:2],classes)
-print np.mean(pca.explained_variance_ratio_)
-pca.fit(data[:,1:3],classes)
-print np.mean(pca.explained_variance_ratio_)
+pca = PCA()
+pca.fit(data[:,:2])
+print np.sum(np.sqrt(pca.explained_variance_))
+pca.fit(data[:,1:3])
+print np.sum(np.sqrt(pca.explained_variance_))
 
+#Rysowanie wykresu
 pylab.plot(x, y)
 pylab.title('Zaleznosc skumulowanego odchylenia standardowego od liczby zmiennych')
 pylab.xlabel('Liczba zmiennych')
